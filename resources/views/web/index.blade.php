64d411bee5abfa2c889afcfe2625b093c54731e5
Yonathan Castillo <leothan522@gmail.com>
2023-05-30 10:31:49 -0400

    Pagina de inicio, mensaje de bienvenida

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -9,3 +9,5 @@
 @section('footer_direccion', 'Edificio “Cilinia” piso 3')
 @section('footer_ciudad', 'San Juan de los Morros')
 @section('footer_estado', 'Estado Guárico')
+@section('hero_title', 'Bienvenido')
+@section('hero_message', 'Puedes interactuar de forma fácil y sencilla en un chat público, directamente desde nuestra propia App Android. Completamente gratis. ')
