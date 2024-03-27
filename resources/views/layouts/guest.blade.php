9f0f4704b1b71b01146acd1fb54d1eb7fd71fb1b
Yonathan Castillo <leothan522@gmail.com>
2024-03-27 12:08:26 -0400

    Actualización de Jetstream 3.x a Jetstream 4.x

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -33,11 +33,13 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
        {{-- <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script>--}}
+        @livewireStyles
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
         @stack('scripts')
+        @livewireScripts
     </body>
 </html>
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -40,7 +40,7 @@
 
     {{-- Livewire Styles --}}
     @if(config('adminlte.livewire'))
-        @if(app()->version() >= 7)
+        @if(intval(app()->version()) >= 7)
             @livewireStyles
         @else
             <livewire:styles />
@@ -77,35 +77,36 @@
 
 <body class="@yield('classes_body')" @yield('body_data')>
 
-{{-- Body Content --}}
-@yield('body')
+    {{-- Body Content --}}
+    @yield('body')
 
-{{-- Base Scripts --}}
-@if(!config('adminlte.enabled_laravel_mix'))
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
-@else
-    <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
-@endif
-
-{{-- Extra Configured Plugins Scripts --}}
-@include('adminlte::plugins', ['type' => 'js'])
-
-{{-- Livewire Script --}}
-@if(config('adminlte.livewire'))
-    @if(app()->version() >= 7)
-        @livewireScripts
+    {{-- Base Scripts --}}
+    @if(!config('adminlte.enabled_laravel_mix'))
+        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+        <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     @else
-        <livewire:scripts />
+        <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
     @endif
-@endif
 
-{{-- Custom Scripts --}}
-@yield('adminlte_js')
-@include('sweetalert::alert')
-<x-livewire-alert::scripts />
+    {{-- Extra Configured Plugins Scripts --}}
+    @include('adminlte::plugins', ['type' => 'js'])
+
+    {{-- Livewire Script --}}
+    @if(config('adminlte.livewire'))
+        @if(intval(app()->version()) >= 7)
+            @livewireScripts
+        @else
+            <livewire:scripts />
+        @endif
+    @endif
+
+    {{-- Custom Scripts --}}
+    @yield('adminlte_js')
+    @include('sweetalert::alert')
+    <x-livewire-alert::scripts />
+
 </body>
 
 </html>
